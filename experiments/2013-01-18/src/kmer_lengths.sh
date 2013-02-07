7fe49dcd77388807b97cb21e4e53e186fe762c16
Johannes Alneberg <alneberg@kth.se>
2013-02-07 17:31:51 +0100

    Changed in main script to adapt to recent changes

--- experiments/2013-01-18/src/kmer_lengths.sh
+++ experiments/2013-01-18/src/kmer_lengths.sh
@@ -4,6 +4,6 @@
 echo "Parsing the taxonomy file"
 ./parse_taxonomy.py -s 4 -g 4 ../data/gen_tax.tab -o ../results/parsed_gen
 echo "Comparing families"
-./pairwise_probabilities.py -m "multinomial" -k 4 ../results/parsed_gen_families.txt -o ../results/multinomial_prob_families.txt
+./pairwise_probabilities.py -m "multinomial" -k 4 ../results/parsed_gen_families.txt -o ../results/multinomial_prob_families.txt -d /home/johannes/repos/DATA/referenc_genomes_ncbi -c 100 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
 echo "Comparing genera"
 ./pairwise_probabilities.py -m "multinomial" -k 4 ../results/parsed_gen_genera.txt -o ../results/multinomial_prob_genera.txt
