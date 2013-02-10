04c01d7e75b5af6f7805b5ac1a8cbc3ddbad394a
Johannes Alneberg <alneberg@kth.se>
2013-02-10 19:50:59 +0100

    kmer_lengths script adapted to first setting

--- experiments/2013-01-18/src/kmer_lengths.sh
+++ experiments/2013-01-18/src/kmer_lengths.sh
@@ -2,8 +2,6 @@
 # This experiment aim is to compare how different kmer-lengths affect 
 # the genomic profile specificity for different taxonomic levels.
 echo "Parsing the taxonomy file"
-./parse_taxonomy.py -s 4 -g 4 ../data/gen_tax.tab -o ../results/parsed_gen
-echo "Comparing families"
-./pairwise_probabilities.py -m "multinomial" -k 4 ../results/parsed_gen_families.txt -o ../results/multinomial_prob_families.txt -d /home/johannes/repos/DATA/referenc_genomes_ncbi -c 100 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
-echo "Comparing genera"
-./pairwise_probabilities.py -m "multinomial" -k 4 ../results/parsed_gen_genera.txt -o ../results/multinomial_prob_genera.txt
+./parse_taxonomy_complete.py -s 2 -g 2 ../data/gen_tax.tab -o ../results/parsed_gen_2_2
+echo "Calculate multinomial log probabilities for contigs from all genomes"
+./pairwise_probabilities.py -m "multinomial" -k 4 ../results/parsed_gen_2_2_complete.txt -o ../results/multinomial_prob_families.txt -d /home/johannes/repos/DATA/reference_genomes_ncbi -c 100 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
