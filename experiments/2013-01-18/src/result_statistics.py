742b330528dfb8f81313b2e251c3450c11932d86
Johannes Alneberg <alneberg@kth.se>
2013-02-11 17:15:09 +0100

    Results per genome first draft

--- experiments/2013-01-18/src/result_statistics.py
+++ experiments/2013-01-18/src/result_statistics.py
@@ -4,19 +4,32 @@ import sys
 import pandas
 import numpy as np
 
-def main(file_name):
+def main(file_name, output_base):
     df =  pandas.io.parsers.read_table(file_name, sep='\t')
-    # Self-to-self scoring:
     df_sts = pandas.DataFrame()
+    if output_base:
+        sys.stdout = open(output_base + '_species', 'w')
+        
     for specie in df.contig_species.unique():
-        s_specie = df[(df.contig_species == specie) & (df.compare_species == specie)].p_value
-        m_val = s_specie.mean()
-        std_val = np.std(np.array(s_specie))
-        sys.stdout.write(specie + '\t' + str(m_val) +'\t' + str(std_val) + '\n')
+        genus = df[df.contig_species == specie].contig_genus.unique()[0]
+        family = df[df.contig_species == specie].contig_family.unique()[0]
 
-    # Within-genus scoring:
+        # Self-to-self scoring:
+        s_specie = df[(df.contig_species == specie) & (df.compare_species == specie)].p_value
+        m_val_specie = s_specie.mean()
+        std_val_specie = np.std(np.array(s_specie))
+        # Within-genus scoring:
+        s_genus = df[(df.contig_species == specie) & (df.compare_species != specie) & (df.compare_genus == genus)].p_value
+        m_val_genus = s_genus.mean()
+        std_val_genus = np.std(np.array(s_genus))
+        
+        # Within-family scoring:
+        s_family = df[(df.contig_genus != genus) & (df.contig_species != specie) & (df.compare_genus == genus)].p_value
+        m_val_family = s_family.mean()
+        std_val_family = np.std(np.array(s_family))
+        sys.stdout.write(specie + '\t' + str(m_val_specie) +'\t' + str(std_val_specie) + '\t' + str(m_val_genus) + '\t' + str(std_val_specie) +'\t' + str(m_val_family) +'\t' + str(std_val_family) +  '\n')
+
     
-    # Within-family scoring:
 
     # Outside-family scoring:
 
@@ -25,9 +38,7 @@ if __name__=="__main__":
     parser.add_argument('file', 
                         help='specify input file, default is stdin')
     parser.add_argument('-o', '--output', 
-                        help='specify the output file.  The default is stdout')
+                        help='specify the base name of the output files.  The default is stdout')
     args = parser.parse_args()
-    if args.output and args.output != '-':
-        sys.stdout = open(args.output, 'w')
-        
-    main(args.file)
+
+    main(args.file, args.output)
