03840395ce540878b7690eaf62d653c94d28371e
Johannes Alneberg <alneberg@kth.se>
2013-02-13 16:23:27 +0100

    Changed data_path variable

--- experiments/2013-01-18/src/kmer_lengths.sh
+++ experiments/2013-01-18/src/kmer_lengths.sh
@@ -9,19 +9,19 @@
 
 
 DATA_PATH=$HOME"/glob/data"
-#DATA_PATH=$HOME"/repos/DATA/reference_genomes_ncbi"
+#DATA_PATH=$HOME"/repos/DATA"
 
 echo "Parsing the taxonomy file" >&2
-time ./parse_taxonomy_complete.py -s 2 -g 2 ../data/gen_tax.tab -o ../results/parsed_gen_2_2_u
+time ./parse_taxonomy_complete.py -s 2 -g 2 $DATA_PATH"/gen_tax.tab" -o ../results/parsed_gen_2_2_u
 echo "Calculate multinomial log probabilities for contigs from all genomes" >&2
 echo "k=3"
-time ./pairwise_probabilities.py -m "multinomial" -k 3 ../results/parsed_gen_2_2_u_complete.txt -o ../results/multinomial_3_1000 -d $DATA_PATH -c 1000 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
+time ./pairwise_probabilities.py -m "multinomial" -k 3 ../results/parsed_gen_2_2_u_complete.txt -o ../results/multinomial_3_1000 -d $DATA_PATH"/reference_genomes_ncbi" -c 1000 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
 
 echo "k=4"
-time ./pairwise_probabilities.py -m "multinomial" -k 4 ../results/parsed_gen_2_2_u_complete.txt -o ../results/multinomial_4_1000 -d $DATA_PATH -c 1000 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
+time ./pairwise_probabilities.py -m "multinomial" -k 4 ../results/parsed_gen_2_2_u_complete.txt -o ../results/multinomial_4_1000 -d $DATA_PATH"/reference_genomes_ncbi" -c 1000 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
 
 echo "k=5"
-time ./pairwise_probabilities.py -m "multinomial" -k 5 ../results/parsed_gen_2_2_u_complete.txt -o ../results/multinomial_5_1000 -d $DATA_PATH -c 1000 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
+time ./pairwise_probabilities.py -m "multinomial" -k 5 ../results/parsed_gen_2_2_u_complete.txt -o ../results/multinomial_5_1000 -d $DATA_PATH"/reference_genomes_ncbi" -c 1000 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
 
 echo "Stats summary for k=3" >&2
 time ./result_statistics.py ../results/multinomial_3_1000 -o ../results/multinomial_stats_3_1000
