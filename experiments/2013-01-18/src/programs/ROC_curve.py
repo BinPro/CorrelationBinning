146f2c7b608a9c9d28b2395150de42a99f9eb8b6
Johannes Alneberg <alneberg@kth.se>
2013-02-27 13:02:33 +0100

    ROC_curve can handle multiple input files

--- experiments/2013-01-18/src/programs/ROC_curve.py
+++ experiments/2013-01-18/src/programs/ROC_curve.py
@@ -7,27 +7,30 @@ from corrbin.score import ExperimentData
 from corrbin.classification import classify_bool
 import matplotlib.pyplot as plt
 
-def main(input_file, output_file):
-    data = ExperimentData()
-    data.load_data_frame(input_file)
-    data.classify()
-    data.calculate_roc()
+def main(input_files, output_file):
+    counter = 0
+    for input_file in input_files:
+        data = ExperimentData()
+        data.load_data_frame(input_file)
+        data.classify()
+        data.calculate_roc()
     
-    #Plot ROC curve
-    x = data.roc_data.false_positive_rate
-    y = data.roc_data.true_positive_rate
-    plt.plot(x,y)
-    plt.savefig(output_file)
+        # Plot ROC curve
+        x = data.roc_data.false_positive_rate
+        y = data.roc_data.true_positive_rate
+        plt.plot(x,y)
+        plt.savefig(output_file + str(counter))
+        counter += 1
         
 
 
 if __name__=="__main__":
     parser = ArgumentParser()
-    parser.add_argument('file', 
-                        help='specify input file, default is stdin')
+    parser.add_argument('files', nargs='*', 
+                        help='specify input files, default is stdin')
     parser.add_argument('-o', '--output', 
                         help='specify the base name of the output file.  The default is stdout')
 
     args = parser.parse_args()
 
-    main(args.file, args.output)
+    main(args.files, args.output)
