30a55d3fbbf4b4559e90df93c53185444174ba27
Johannes Alneberg <alneberg@kth.se>
2013-03-06 15:42:39 +0100

    Generate contig script

--- experiments/2013-03-04/src/scripts/generate_contigs.sh
+++ experiments/2013-03-04/src/scripts/generate_contigs.sh
@@ -11,10 +11,7 @@
 DATA_PATH=$HOME"/repos/DATA"
 
 #RESULTS_PATH=$HOME"/glob/results/2013-01-18"
-RESULTS_PATH=$HOME"/repos/Correlation-Binning/experiments/2013-03-04/results"
-
-echo "Parsing the taxonomy file" >&2
-time ../programs/parse_taxonomy_complete.py -s 2 -g 2 $DATA_PATH"/gen_tax.tab" -o $RESULTS_PATH"/parsed_gen_2_2_u"
+RESULTS_PATH=$HOME"/repos/DATA/contigs"
 
 echo "Generate contigs" >&2
-time ../programs/generate_contigs.py $RESULTS_PATH"/parsed_gen_2_2_u" -o $RESULTS_PATH"/multinomial_3_1000" -d $DATA_PATH"/reference_genomes_ncbi" -c 1000 -p "genomes" --contig_min_length 1000 --contig_max_length 1000
+time ../programs/generate_contigs.py $DATA_PATH"/parsed_gen_2_2_complete_old.txt" -o $RESULTS_PATH"/contigs_2_2_old_100_100.fna" -d $DATA_PATH"/reference_genomes_ncbi" -c 100 -p "genomes" --contig_min_length 100 --contig_max_length 100
