5faecc2c4c155dbea93d3c709aae5151ed0de4cd
Johannes Alneberg <alneberg@kth.se>
2013-03-10 22:21:37 +0100

    Adjusted argument parser argument for score contigs multinomial

--- experiments/2013-03-08/src/programs/score_contigs_multinomial.py
+++ experiments/2013-03-08/src/programs/score_contigs_multinomial.py
@@ -41,32 +41,21 @@ def main(contigs_file,taxonomy_file, dir_path, kmer_length):
    
 if __name__=="__main__":
     parser = ArgumentParser()
-    parser.add_argument('files', nargs='*', 
-                        help='specify input files, default is stdin')
+    parser.add_argument('contigs', 
+                        help='specify contig file, default is stdin')
     parser.add_argument('-o', '--output', 
                         help='specify the output file.  The default is stdout')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='information written to stderr during execution.')
-    parser.add_argument('-m', '--model', default="multinomial", type=str,
-                        help='specify the model to use for calculating the probabilities, default is multinomial')
+    parser.add_argument('-t', '--taxonomy',
+                        help='specify the taxonomy file.')
     parser.add_argument('-k', '--kmer_length', default=4, type=int,
                         help='specify the kmer length, default is 4')
     parser.add_argument('-d', '--directory_path', default='/home/johannes/repos/DATA/reference_genomes_ncbi', 
                         type=str, help='specify the path to where the reference genomes are located locally')
-    parser.add_argument('-c', '--no_contigs', default=100, type=int,
-                        help='Specify the number of contigs to be sampled from each group. This may be only approximate due to what priority is chosen') 
-    parser.add_argument('-p', '--priority', default="genomes",
-                        type=str, help='specify the prioritized way of sampling contigs. Specify "groups" to make sure each group is sampled exactly the number of times specified by no_contigs, distributed randomly over the genomes present in each group, or specify "genomes" to make sure each genome within a certain group contributes with exactly the same number of contigs.')
-    parser.add_argument('--contig_min_length', default=1000, type=int, help='Specify the minimum length for contigs')
-    parser.add_argument('--contig_max_length', default=1000, type=int, help='Specify the maximum length for contigs')
-    parser.add_argument('--debug_mode', action='store_true', help='In debug mode, all contigs will start at the first nucleotide, making testing possible.')
     args = parser.parse_args()
     if args.output and args.output != '-':
         sys.stdout = open(args.output, 'w')
+    if args.taxonomy:
+        taxonomy_file = open(args.taxonomy, 'r')
         
-    name_file_handle = fileinput.input(args.files)
-    if args.verbose:
-        sys.stderr.write("Number of genomes read: %i %s" % (len(genomes),os.linesep))
-    ex_setting = SampleSetting(args.priority, args.no_contigs, args.contig_min_length, args.contig_max_length, args.debug_mode)
-    main(name_file_handle, args.directory_path, args.kmer_length, ex_setting)
-    name_file_handle.close()
+    main(args.contigs, taxonomy_file, args.directory_path, args.kmer_length)
+
