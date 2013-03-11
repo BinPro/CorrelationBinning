fd6b5c0d67e9779f2efd2b381831662f40539755
Johannes Alneberg <alneberg@kth.se>
2013-03-11 15:35:28 +0100

    score contigs multinomial uppmax script

--- experiments/2013-03-08/src/scripts/score_contigs_multinomial.sh
+++ experiments/2013-03-08/src/scripts/score_contigs_multinomial.sh
@@ -6,10 +6,17 @@
 # This experiment aim is to compare how different kmer-lengths affect
 # the genomic profile specificity for different taxonomic levels.
 
-#DATA_PATH=$HOME"/glob/data"
-DATA_PATH=$HOME"/repos/DATA"
+DATA_PATH="/bubo/home/h20/brynjar/glob/masterproject/DATA/2013-03-05"
+#DATA_PATH=$HOME"/repos/DATA"
 
 RESULTS_PATH=$HOME"/glob/results/2013-03-08"
 
-echo "Scoring contigs" >&2
-time ../programs/score_contigs_multinomial.py $DATA_PATH"/parsed
+#echo "Scoring contigs 3 100" >&2
+#time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_100.fna" -o $RESULTS_PATH"/score_mul_2_2_100_100.tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k 3 -d $DATA_PATH"/all_fna"
+
+echo "Scoring contigs 3 1000" >&2
+time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_1000.fna" -o $RESULTS_PATH"/score_mul_2_2_100_1000.tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k 3 -d $DATA_PATH"/all_fna"
+
+echo "Scoring contigs 3 10000" >&2
+time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_10000.fna" -o $RESULTS_PATH"/score_mul_2_2_100_10000.tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k 3 -d $DATA_PATH"/all_fna"
+
