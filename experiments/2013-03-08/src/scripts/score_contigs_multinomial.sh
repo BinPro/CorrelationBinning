024bcd93f4a23109cd1d5bc63d0b031b39896df6
Johannes Alneberg <alneberg@kth.se>
2013-03-11 16:11:37 +0100

    KMER_LENGTH variable in score_contigs_multinomial script

--- experiments/2013-03-08/src/scripts/score_contigs_multinomial.sh
+++ experiments/2013-03-08/src/scripts/score_contigs_multinomial.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -A b2010008
 #SBATCH -p node
-#SBATCH -t 8:00:00
+#SBATCH -t 1-00:00:00
 
 # This experiment aim is to compare how different kmer-lengths affect
 # the genomic profile specificity for different taxonomic levels.
@@ -11,12 +11,14 @@ DATA_PATH="/bubo/home/h20/brynjar/glob/masterproject/DATA/2013-03-05"
 
 RESULTS_PATH=$HOME"/glob/results/2013-03-08"
 
-#echo "Scoring contigs 3 100" >&2
-#time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_100.fna" -o $RESULTS_PATH"/score_mul_2_2_100_100.tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k 3 -d $DATA_PATH"/all_fna"
+KMER_LENGTH=7
 
-echo "Scoring contigs 3 1000" >&2
-time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_1000.fna" -o $RESULTS_PATH"/score_mul_2_2_100_1000.tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k 3 -d $DATA_PATH"/all_fna"
+echo "Scoring contigs, length 100, kmer length "$KMER_LENGTH >&2
+time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_100.fna" -o $RESULTS_PATH"/score_mul_2_2_100_100_$KMER_LENGTH"".tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k $KMER_LENGTH -d $DATA_PATH"/all_fna"
 
-echo "Scoring contigs 3 10000" >&2
-time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_10000.fna" -o $RESULTS_PATH"/score_mul_2_2_100_10000.tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k 3 -d $DATA_PATH"/all_fna"
+echo "Scoring contigs, length 1000, kmer length "$KMER_LENGTH >&2
+time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_1000.fna" -o $RESULTS_PATH"/score_mul_2_2_100_1000_$KMER_LENGTH"".tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k $KMER_LENGTH -d $DATA_PATH"/all_fna"
+
+echo "Scoring contigs, length 10000, kmer_length "$KMER_LENGTH >&2
+time ../programs/score_contigs_multinomial.py $DATA_PATH"/contigs/contigs_2_2_new_100_10000.fna" -o $RESULTS_PATH"/score_mul_2_2_100_10000_$KMER_LENGTH"".tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k $KMER_LENGTH -d $DATA_PATH"/all_fna"
 
