cd8d72a8587daf60be84ac49e0aa215b5ab35004
Johannes Alneberg <alneberg@kth.se>
2013-03-21 11:06:49 +0100

    Matrix row script loops over kmer_lengths

--- experiments/2013-03-12/src/scripts/matrix_rows.sh
+++ experiments/2013-03-12/src/scripts/matrix_rows.sh
@@ -11,7 +11,12 @@ DATA_PATH=$HOME"/repos/RESULTS/to_big_for_git/2013-03-08/links_to_best"
 RESULTS_PATH=$HOME"/repos/RESULTS/multinomial_kmer_length/precision_matrix/2013-03-20"
 
 LEVEL=$1
-KMER_LENGTH=$2
-
-echo "calculating matrix row, precision for "$LEVEL" level, contig length 100,1000 and 10000, kmer="$KMER_LENGTH
-time ../programs/matrix_row.py $DATA_PATH"/score_mul_2_2_100_100_"$KMER_LENGTH".tsv" $DATA_PATH"/score_mul_2_2_100_1000_"$KMER_LENGTH".tsv" $DATA_PATH"/score_mul_2_2_100_10000_"$KMER_LENGTH".tsv"  -o $RESULTS_PATH"/m_row_100_1000_10000_100_"$KMER_LENGTH"_"$LEVEL --level $LEVEL -f "precision"
+KMER_MIN=$2
+KMER_MAX=$3
+KMER_LENGTH=$KMER_MIN
+while [[ $KMER_LENGTH -le $KMER_MAX ]]
+do
+    echo "calculating matrix row, precision for "$LEVEL" level, contig length 100,1000 and 10000, kmer="$KMER_LENGTH
+    time ../programs/matrix_row.py $DATA_PATH"/score_mul_2_2_100_100_"$KMER_LENGTH".tsv" $DATA_PATH"/score_mul_2_2_100_1000_"$KMER_LENGTH".tsv" $DATA_PATH"/score_mul_2_2_100_10000_"$KMER_LENGTH".tsv"  -o $RESULTS_PATH"/m_row_100_1000_10000_100_"$KMER_LENGTH"_"$LEVEL --level $LEVEL -f "precision"
+    ((KMER_LENGTH = KMER_LENGTH + 1))
+done
