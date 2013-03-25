605ce59e947a2c62637f1ecd5f27def18010a2a0
Johannes Alneberg <alneberg@kth.se>
2013-03-25 16:30:22 +0100

    Basic structure for dirichlet scoring script

---
+++ experiments/2013-03-11/src/scripts/score_contigs_dirichlet.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#SBATCH -A b2010008
+#SBATCH -p node
+#SBATCH -t 3-00:00:00
+
+# This experiment aim is to compare how different kmer-lengths affect
+# the genomic profile specificity for different taxonomic levels.
+
+DATA_PATH="/bubo/home/h20/brynjar/glob/masterproject/DATA/2013-03-05"
+#DATA_PATH=$HOME"/repos/DATA"
+
+RESULTS_PATH=$HOME"/glob/results/2013-03-08/2013-03-18"
+
+KMER_LENGTH=3
+
+echo "Scoring contigs, length 100, kmer length "$KMER_LENGTH >&2
+time ../programs/score_contigs_dirichlet.py $DATA_PATH"/contigs/contigs_2_2_new_100_100.fna" -o $RESULTS_PATH"/score_mul_2_2_100_100_$KMER_LENGTH"".tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k $KMER_LENGTH -d $DATA_PATH"/all_fna"
+
+echo "Scoring contigs, length 1000, kmer length "$KMER_LENGTH >&2
+time ../programs/score_contigs_dirichlet.py $DATA_PATH"/contigs/contigs_2_2_new_100_1000.fna" -o $RESULTS_PATH"/score_mul_2_2_100_1000_$KMER_LENGTH"".tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k $KMER_LENGTH -d $DATA_PATH"/all_fna"
+
+echo "Scoring contigs, length 10000, kmer_length "$KMER_LENGTH >&2
+time ../programs/score_contigs_dirichlet.py $DATA_PATH"/contigs/contigs_2_2_new_100_10000.fna" -o $RESULTS_PATH"/score_mul_2_2_100_10000_$KMER_LENGTH"".tsv" -t $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -k $KMER_LENGTH -d $DATA_PATH"/all_fna"
+
