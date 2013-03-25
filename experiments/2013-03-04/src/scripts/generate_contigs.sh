3c0b753efffef74a64f62b97f8b669a9d040667f
Johannes Alneberg <alneberg@kth.se>
2013-03-25 12:32:54 +0100

    Used generate contigs script

--- experiments/2013-03-04/src/scripts/generate_contigs.sh
+++ experiments/2013-03-04/src/scripts/generate_contigs.sh
@@ -7,12 +7,11 @@
 # evaluation. The output is saved in order to be reused for all 
 # experiments.
 
-DATA_PATH="/bubo/home/h20/brynjar/glob/masterproject/DATA/2013-03-05"
+#DATA_PATH="/bubo/home/h20/brynjar/glob/masterproject/DATA/2013-03-05"
+DATA_PATH=$HOME"/repos/DATA/2013-03-05"
 
-#DATA_PATH=$HOME"/repos/DATA"
-
-RESULTS_PATH="/bubo/home/h20/brynjar/glob/masterproject/DATA/2013-03-05/contigs"
-#RESULTS_PATH=$HOME"/repos/DATA/contigs"
+#RESULTS_PATH="/bubo/home/h20/brynjar/glob/masterproject/DATA/2013-03-05/contigs"
+RESULTS_PATH=$HOME"/repos/RESULTS/contigs/start_position"
 
 echo "Generate contigs" >&2
-time ../programs/generate_contigs.py $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -o $RESULTS_PATH"/contigs_2_2_new_100_10000.fna" -d $DATA_PATH"/all_fna" -c 100 -p "genomes" --contig_min_length 10000 --contig_max_length 10000
+time ../programs/generate_contigs.py $DATA_PATH"/parsed_taxonomy_2_2_2013-03-05_complete.txt" -o $RESULTS_PATH"/contigs_2_2_100_100_start_position.fna" -d $DATA_PATH"/all_fna" -c 100 -p "genomes" --contig_min_length 100 --contig_max_length 100 --start_position
