368d1cfe27330538ef5f9ca373b044897aa40054
Johannes Alneberg <alneberg@kth.se>
2013-04-08 17:06:11 +0200

    Shuffle output from trim time series

--- experiments/2013-04-05/src/programs/trim_time_series.py
+++ experiments/2013-04-05/src/programs/trim_time_series.py
@@ -2,6 +2,7 @@
 from argparse import ArgumentParser
 import fileinput
 import numpy as np
+from random import shuffle
 import pandas as p
 from corrbin.score import ExperimentData, RocAxisFuns
 from corrbin.classification import classify_bool
@@ -14,13 +15,15 @@ def main(input_file, first_data, last_data,output_file, number_of_genomes):
     column_sums = np.sum(full_df.ix[:,first_data:last_data].values, axis=0)
     for index, column_name in enumerate(columns):
         full_df[column_name]/=float(column_sums[index])
-    
+
     totals = p.Series(np.sum(full_df.ix[:,first_data:last_data].values,axis=1),index=full_df.index)
     totals.sort()
-    filtered_df = full_df.ix[totals[-number_of_genomes:].index]
+    included_indices = list(totals[-number_of_genomes:].index)
+    
+    shuffle(included_indices)
+    filtered_df = full_df.ix[included_indices]
     filtered_df.to_csv(output_file,sep='\t')
 
-    
 
 if __name__=="__main__":
     parser = ArgumentParser()
