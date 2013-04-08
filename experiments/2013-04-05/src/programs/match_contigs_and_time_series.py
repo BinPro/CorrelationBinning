8d6cfb34850f7d295acdb2fff62704e90beb15e6
Johannes Alneberg <alneberg@kth.se>
2013-04-08 13:54:11 +0200

    time_series: load time series and contigs, match real with contigs

--- experiments/2013-04-05/src/programs/match_contigs_and_time_series.py
+++ experiments/2013-04-05/src/programs/match_contigs_and_time_series.py
@@ -5,35 +5,61 @@ import numpy as np
 import pandas as p
 from corrbin.score import ExperimentData, RocAxisFuns
 from corrbin.classification import classify_bool
+from corrbin.io import read_contigs_file, print_contigs_time_series
 import matplotlib.pyplot as plt
 import sys
+import probin.dna as dna
 
-def main(time_series_file, contig_file, first_data, last_data,output_file, number_of_genomes):
+
+def main(time_series_file, contig_file, output_file, contig_length, number_of_reads, first_data, last_data):
+    dna.DNA.generate_kmer_hash(2)
     time_series_df = p.io.parsers.read_table(time_series_file,sep='\t')
     contigs = read_contigs_file(contig_file)
-    
-    for index, column_name in enumerate(columns):
-        full_df[column_name]/=float(column_sums[index])
-    
-    totals = p.Series(np.sum(full_df.ix[:,first_data:last_data].values,axis=1),index=full_df.index)
-    totals.sort()
-    filtered_df = full_df.ix[totals[-number_of_genomes:].index]
-    filtered_df.to_csv(output_file,sep='\t')
 
+    # Match contigs with a real time_series
+    ##  Count number of contigs per genome
+    genomes = [c.id for c in contigs]
+    genomes.sort()
+
+    ##  Match time series index with
+    ts_index_and_count = zip(time_series_df.index,[genomes.count(g) for g in genomes])
+    multiplied_time_series = []
+    for time_series_i,count in ts_index_and_count:
+        multiplied_time_series += [time_series_i]*count
+
+    # Simulate new time series for each contig, with the real as base.
+    for contig,ts_i in zip(contigs,multiplied_time_series):
+        ts = time_series_df.ix[ts_i]
+        contig.otu = ts["# OTU"]
+        contig.time_series = ts[first_data:last_data]
     
+    
+    sample_headers =time_series_df.ix[:,first_data:last_data].columns
+    print_contigs_time_series(contigs,output_file,sample_headers)
+
 
 if __name__=="__main__":
     parser = ArgumentParser()
     parser.add_argument('file',
-                        help='specify input file, default is stdin')
-    parser.add_argument('--first_data', 
-                        help='specify the first data column')
-    parser.add_argument('--last_data', 
-                        help='specify the last data column')
+                        help='specify input time series file, default is stdin')
+    parser.add_argument('--contigs_file',
+                        help='specify input contigs file')
     parser.add_argument('-o', '--output', 
                         help='specify the base name of the output file.  The default is stdout')
-    parser.add_argument('--number_of_genomes', type=int,
-                        help='specify the number of data rows to be extracted')
+    parser.add_argument('--contig_length', type=int,
+                        help='specify the length of contigs')
+    parser.add_argument('--number_of_reads',type=int,
+                        help='speciy total number of reads for a single sample')
+    parser.add_argument('--first_data',
+                        help='specify first data point for the samples')
+    parser.add_argument('--last_data',
+                        help='specify last data point for the samples')
     args = parser.parse_args()
 
-    main(args.file, args.first_data, args.last_data,args.output, args.number_of_genomes)
+    if args.output:
+        output_file = open(args.output,'w+')
+    else:
+        output_file = sys.stdout
+    main(args.file,args.contigs_file, output_file, args.contig_length, args.number_of_reads, args.first_data, args.last_data)
+
+    output_file.close()
