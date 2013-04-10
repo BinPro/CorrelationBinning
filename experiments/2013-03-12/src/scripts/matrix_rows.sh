2de00fd45f37aa26ca754e97ec8f4fa29edd25dc
Johannes Alneberg <alneberg@kth.se>
2013-04-10 09:19:33 +0200

    Matrix row for dirichlet instead of multinomial

--- experiments/2013-03-12/src/scripts/matrix_rows.sh
+++ experiments/2013-03-12/src/scripts/matrix_rows.sh
@@ -5,10 +5,11 @@
 #SBATCH -t 1-00:00:00
 
 #DATA_PATH="/bubo/home/h20/brynjar/glob/masterproject/DATA/2013-03-05"
-DATA_PATH=$HOME"/repos/RESULTS/to_big_for_git/2013-03-08/links_to_best"
+#DATA_PATH=$HOME"/repos/RESULTS/to_big_for_git/2013-03-11/2013-03-26"
+DATA_PATH=$HOME"/glob/RESULTS/to_big_for_git/2013-03-11/2013-03-26"
 
 #RESULTS_PATH=$HOME"/glob/results/2013-03-08"
-RESULTS_PATH=$HOME"/repos/RESULTS/multinomial_kmer_length/precision_matrix/2013-03-20"
+RESULTS_PATH=$HOME"/glob/RESULTS/dirichlet_multinomial_kmer_length/precision_matrix/2013-03-26"
 
 LEVEL=$1
 KMER_MIN=$2
@@ -17,6 +18,6 @@ KMER_LENGTH=$KMER_MIN
 while [[ $KMER_LENGTH -le $KMER_MAX ]]
 do
     echo "calculating matrix row, precision for "$LEVEL" level, contig length 100,1000 and 10000, kmer="$KMER_LENGTH
-    time ../programs/matrix_row.py $DATA_PATH"/score_mul_2_2_100_100_"$KMER_LENGTH".tsv" $DATA_PATH"/score_mul_2_2_100_1000_"$KMER_LENGTH".tsv" $DATA_PATH"/score_mul_2_2_100_10000_"$KMER_LENGTH".tsv"  -o $RESULTS_PATH"/m_row_100_1000_10000_100_"$KMER_LENGTH"_"$LEVEL --level $LEVEL -f "precision"
+    time ../programs/matrix_row.py $DATA_PATH"/score_dirichlet_2_2_100_100_"$KMER_LENGTH".tsv" $DATA_PATH"/score_dirichlet_2_2_100_1000_"$KMER_LENGTH".tsv" $DATA_PATH"/score_dirichlet_2_2_100_10000_"$KMER_LENGTH".tsv"  -o $RESULTS_PATH"/m_row_100_1000_10000_100_"$KMER_LENGTH"_"$LEVEL --level $LEVEL -f "precision"
     ((KMER_LENGTH = KMER_LENGTH + 1))
 done
