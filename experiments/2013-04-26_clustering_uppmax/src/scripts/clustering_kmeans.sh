3c28fc6a3510f1203aba5004a41aa98977a1a0d6
Johannes Alneberg <alneberg@kth.se>
2013-04-25 17:06:59 +0200

    Clustering kmeans script ready to be ran on uppmax

--- experiments/2013-04-26_clustering_uppmax/src/scripts/clustering_kmeans.sh
+++ experiments/2013-04-26_clustering_uppmax/src/scripts/clustering_kmeans.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -A b2010008
-#SBATCH -p node
-#SBATCH -t 1-00:00:00
+#SBATCH -p devel
+#SBATCH -t 1:00:00
 #SBATCH -J Clustering_kmeans
 
 # This experiment aim is to compare how different kmer-lengths affect
@@ -10,14 +10,17 @@
 DATA_PATH="/proj/b2010008/ProBin/DATA/2013-04-26"
 #DATA_PATH=$HOME"/repos/DATA"
 
-RESULTS_PATH=$HOME"/proj/b2010008/ProBin/RESULTS/2013-04-26"
+RESULTS_PATH="/proj/b2010008/ProBin/RESULTS/clustering/2013-04-26"
 mkdir -p $RESULTS_PATH
 
-KMER_LENGTH=3
-CLUSTER_COUNT=3
+KMER_LENGTH=4
+CLUSTER_COUNT=7
 CLUSTER_ALG="kmeans"
 
-COMMAND=ProBin.py $DATA_PATH/........ -k $KMER_LENGTH -c $CLUSTER_COUNT -a $CLUSTER_ALG -o $RESULT_PATH/$CLUSTER_ALG"_"$KMER_LENGTH"_"$CLUSTER_COUNT".csv" 
+OUTPUT_CSV=$RESULTS_PATH/$CLUSTER_ALG"_"$KMER_LENGTH"_"$CLUSTER_COUNT".csv" 
+OUTPUT_ERROR=$RESULTS_PATH/$CLUSTER_ALG"_"$KMER_LENGTH"_"$CLUSTER_COUNT".error_log" 
+
+COMMAND="ProBin.py bin $DATA_PATH/generated_contigs_10000_test.fna -k $KMER_LENGTH -c $CLUSTER_COUNT -a $CLUSTER_ALG -o $OUTPUT_CSV"
 
 echo $COMMAND
 time $COMMAND
