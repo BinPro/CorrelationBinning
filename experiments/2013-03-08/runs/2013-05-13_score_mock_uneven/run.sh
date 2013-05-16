5d2e544d312eb4518f0abfe163831bc92acf2d70
Johannes Alneberg <alneberg@kth.se>
2013-05-16 11:17:30 +0200

    Fixed erroneous way of scoring mock data for multinomial

--- experiments/2013-03-08/runs/2013-05-13_score_mock_uneven/run.sh
+++ experiments/2013-03-08/runs/2013-05-13_score_mock_uneven/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -A b2010008
-#SBATCH -p devel
-#SBATCH -t 1:00:00
+#SBATCH -p node
+#SBATCH -t 1-00:00:00
 
-make score_mul_0_0_mock_2.tsv
+CMD="make score_mul_0_0_mock_"$1".tsv"
+$CMD
