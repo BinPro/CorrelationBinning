4bd5c31dd52ff47b20fb1198dac783cb7de805f8
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-05-29 15:30:36 +0200

    fixing calculating statistics

--- src/corrbin/cluster_statistics.py
+++ src/corrbin/cluster_statistics.py
@@ -7,26 +7,40 @@ import pandas as pd
 from pandas import DataFrame, Series, pivot_table
 import numpy as np
 from itertools import izip
+from argparse import ArgumentParser
 
-def get_phylo_from_file(path):
+def get_phylo_from_file(phylo_file):
     """
     File in path is a comma seperated file with columns: contig_id, family, genus, species
     """
-    return pd.read_csv(path)
+    return pd.read_csv(phylo_file)
 
-def get_clusters(paths):
-    
+def get_clusters_from_file(cluster_file):
+    """
+    File with the output from ProBin execution. Basically, the clusters should be represented as follows:
+    Cluster 0, contig1, contig2, ...
+    Cluster 1, contig1X, contig2X, ...
+    ...
+    """
+    return pd.read_csv(cluster_file)
 
-def get_statistics(contigs,clusters,output):
-    _get_phylo(contigs)    
-    cm = confusion_matrix(contigs,clusters)
-    rc = recall(contigs,clusters)
-    pr = precision(contigs,clusters)
-    for c,r,p in izip(cm,rc,pr):
-        print>>output,"#{0}".format("="*20)
-        c.to_csv(output)
-        r.recall.to_csv(output)
-        p.precision.to_csv(output)
+def get_statistics(cluster_file, phylo_file,level):
+    clusters = get_clusters_from_file(cluster_file)
+    phylo = get_phylo_from_file(phylo_file)
+
+    print phylo
+    print clusters
+
+
+#    _get_phylo(contigs)    
+#    cm = confusion_matrix(contigs,clusters)
+#    rc = recall(contigs,clusters)
+#    pr = precision(contigs,clusters)
+#    for c,r,p in izip(cm,rc,pr):
+#        print>>output,"#{0}".format("="*20)
+#        c.to_csv(output)
+#        r.recall.to_csv(output)
+#        p.precision.to_csv(output)
 
 def confusion_matrix(contigs,clustering,levels=["family","genus","species"]):
     df = _create_dataframe(contigs,clustering)
@@ -47,22 +61,14 @@ def precision(contigs,clustering):
         precision["precision"] = precision.max(axis=1)
     return precisions
 
-def _create_dataframe(contigs,clustering):
-    rows = []
-    for contig in contigs:
-        cluster = None
-        for i,c in enumerate(clustering):
-            if contig in c:
-                if cluster != None:
-                    raise Exception("Contig in multiple clusters, not good!")
-                cluster = i
-                break
-        if cluster == None:
-            raise Exception("Contig not in any cluster, not good!")
-        serie = Series([contig.phylo_family,contig.phylo_genus,contig.phylo_species,cluster,len(contig.full_seq)],index=["family","genus","species","cluster","contig_size"],name=contig.id)
-        rows.append(serie)
-    return DataFrame(rows)
-
-def _get_phylo(contigs):
-    for contig in contigs:
-        (contig.phylo_family, contig.phylo_genus, contig.phylo_species) = contig.phylo.split("|")
+if __name__=="__main__":
+    parser = ArgumentParser(description="Clustering of metagenomic contigs")
+    parser.add_argument('cluster_file', 
+        help='Result file from ProBin program (or each line on the form Cluster X, contig1X, contig2X,..)')
+    parser.add_argument('phylo_file', 
+        help='Phylogenetic file for the cluster_file (each line on the form: contig_id, family, genus, species')
+    parser.add_argument('-l', '--level', 
+        default='family', type=str, choices=['family','genus','species'],
+        help='Calculate statistics for which level.')
+    args = parser.parse_args()
+    get_statistics(args.cluster_file,args.phylo_file,args.level)
