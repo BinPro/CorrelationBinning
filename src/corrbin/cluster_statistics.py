48276f97699d8fd4d23bf00bfd5995a9647d3e04
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-05-29 20:29:31 +0200

    basic stats

--- src/corrbin/cluster_statistics.py
+++ src/corrbin/cluster_statistics.py
@@ -1,35 +1,70 @@
+#!/usr/bin/env python
 """
 Created on Thu Mar 28 10:07:24 2013
 Calculate recall and precision for clustering algorithms. Clustering 
 @author: Brynjar Smari Bjarnason
 """
-import pandas as pd
-from pandas import DataFrame, Series, pivot_table
+import sys
+import os
 import numpy as np
-from itertools import izip
+from pandas import DataFrame, Series, pivot_table, read_csv
+
+
 from argparse import ArgumentParser
 
 def get_phylo_from_file(phylo_file):
     """
-    File in path is a comma seperated file with columns: contig_id, family, genus, species
+    phylo_file: File in path is a comma seperated file with columns: contig_id, family, genus, species
+                family      genus       species    
+    contig1     family1     genus1      species1
+    contig2     family1     genus2      species2
+    contig1X    family2     genus3      species3
+    contig2X    family2     genus3      species4
+    ...
     """
-    return pd.read_csv(phylo_file)
+    df = read_csv(phylo_file,index_col=0)
+    return df
 
 def get_clusters_from_file(cluster_file):
     """
-    File with the output from ProBin execution. Basically, the clusters should be represented as follows:
+    cluster_file: File with the output from ProBin execution. Basically, 
+    the clusters should be represented as follows:
     Cluster 0, contig1, contig2, ...
     Cluster 1, contig1X, contig2X, ...
     ...
+    result: Padas data frame with column cluster and the contig ids as indexes.
+                cluster    
+    contig1     cluster0
+    contig2     cluster0
+    contig1X    cluster1
+    contig2X    cluster1
+    ...
     """
-    return pd.read_csv(cluster_file)
+    index=[]
+    values=[]
+    with open(cluster_file) as fh:
+        for clustering in fh.readlines():
+            if not clustering.startswith("#"):
+                s = clustering.strip().split(",")
+                cluster = s[0]
+                contigs = s[1:]
+                for contig in contigs:
+                    index.append(contig)
+                    values.append(cluster)
+    clustering = DataFrame(Series(values,index=index),columns=["cluster"])
+                
+    return clustering
 
 def get_statistics(cluster_file, phylo_file,level):
     clusters = get_clusters_from_file(cluster_file)
     phylo = get_phylo_from_file(phylo_file)
-
-    print phylo
-    print clusters
+    if len(clusters) != len(phylo):
+        print >> sys.stderr, "not equally many contigs ins clustering and phylo"
+        sys.exit(-1)
+    #we can do this join on the dataframes since the indexes are the same contigs!
+    phylo_clusters = phylo.join(clusters)
+    
+    return phylo_clusters
 
 
 #    _get_phylo(contigs)    
@@ -42,7 +77,8 @@ def get_statistics(cluster_file, phylo_file,level):
 #        r.recall.to_csv(output)
 #        p.precision.to_csv(output)
 
-def confusion_matrix(contigs,clustering,levels=["family","genus","species"]):
+def confusion_matrix(contigs,clustering,level="family"):
+    levels = ["family","genus","species"]
     df = _create_dataframe(contigs,clustering)
     cm = [pivot_table(df,rows=levels[:i+1],cols=["cluster"],aggfunc=np.sum) for i in xrange(len(levels))]
     return cm
@@ -65,10 +101,11 @@ if __name__=="__main__":
     parser = ArgumentParser(description="Clustering of metagenomic contigs")
     parser.add_argument('cluster_file', 
         help='Result file from ProBin program (or each line on the form Cluster X, contig1X, contig2X,..)')
-    parser.add_argument('phylo_file', 
+    parser.add_argument('phylo_file',
         help='Phylogenetic file for the cluster_file (each line on the form: contig_id, family, genus, species')
     parser.add_argument('-l', '--level', 
         default='family', type=str, choices=['family','genus','species'],
         help='Calculate statistics for which level.')
     args = parser.parse_args()
-    get_statistics(args.cluster_file,args.phylo_file,args.level)
+    phylo_clusters = get_statistics(args.cluster_file,args.phylo_file,args.level)
+    phylo_clusters.to_csv("/home/binni/MasterProject/Drasl/{0}".format(os.))
