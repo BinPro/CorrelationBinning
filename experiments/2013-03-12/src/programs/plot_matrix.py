9a60028ab61e8edfaec2ec915d454c0c55d0f4e7
Johannes Alneberg <alneberg@kth.se>
2013-06-25 12:45:43 +0200

    Modified plot matrix procedure

--- experiments/2013-03-12/src/programs/plot_matrix.py
+++ experiments/2013-03-12/src/programs/plot_matrix.py
@@ -6,22 +6,57 @@ import pandas as p
 from corrbin.score import ExperimentData, RocAxisFuns
 from corrbin.classification import classify_bool
 import matplotlib.pyplot as plt
+import sys
 
-def main(input_files, output_file, title,together):
-    fig = plt.figure()
+def main(input_files, output_file, title,together,mock_multiple_samples):
     if together:
+        fig = plt.figure(figsize=(15,6))
+        lines =[]
+        labels = []
         for ix,input_file in enumerate(input_files):
-            ax = plt.subplot(eval(int("13"+ ix)))
+            if ix == 0:
+                ax0 = plt.subplot(1,3,ix+1)
+                ax = ax0
+            else:
+                ax = plt.subplot(1,3,ix+1,sharey=ax0)
             df = p.io.parsers.read_table(input_file, sep=' & ')
-            
-            for contig_l in ['100','1000','10000']:
-                # Plot ROC curve
+
+            linestyles = [':','--','-.']
+            for contig_nr,contig_l in enumerate(['100','1000','10000']):
                 x = df.kmer_length.values
                 y = df[contig_l].values
                 
                 label = "Contig Length: " + contig_l
-                plt.plot(x,y,label=label)
+                labels.append(label)
+                line = plt.plot(x,y)
+                lines.append(line)
+                plt.setp(line,linestyle=linestyles[contig_nr],linewidth=2.0, label=label)
+            if ix == 2:
+                plt.setp(ax.get_yticklabels(),visible=False)
+                plt.setp(ax.get_xticklabels(),fontsize=14)
+            elif ix == 1:
+                plt.setp(ax.get_yticklabels(),visible=False)
+                plt.setp(ax.get_xticklabels(),fontsize=14)
+                plt.xlabel("Kmer Length",fontsize=16)           
+            else:
+                plt.setp(ax.get_yticklabels(),fontsize=14)
+                plt.setp(ax.get_xticklabels(),fontsize=14)
+                plt.ylabel("Precision",fontsize=16)
+
+
+            plt.ylim(0.0,1.0)
+            box = ax.get_position()
+            ax.set_position([box.x0,box.y0,box.width,box.height*0.8])
+
+    elif mock_multiple_samples:
+        fig = plt.figure()
+        ax = plt.subplot(111)
+        df = p.io.parsers.read_table(input_file,sep=' & ')
+        x = df.nr_samples.values
+        y = df.values
+        plt.plot(x,y)
     else:
+        fig = plt.figure()
         ax = plt.subplot(111)
         for input_file in input_files:
             df = p.io.parsers.read_table(input_file, sep=' & ')
@@ -33,14 +68,9 @@ def main(input_files, output_file, title,together):
                 
                 label = "Contig Length: " + contig_l
                 plt.plot(x,y,label=label)
-        plt.ylim(0.0,1.0)
-        plt.title(title)
-        box = ax.get_position()
-        ax.set_position([box.x0,box.y0,box.width,box.height*0.8])
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.35),
-          ncol=2, fancybox=True, shadow=True)
-    plt.xlabel("Kmer Length")
-    plt.ylabel("Precision")
+
+    plt.legend(loc='upper center', bbox_to_anchor=(-0.7, 1.25),
+               ncol=2, fancybox=True, shadow=True)
     plt.savefig(output_file)
     
         
@@ -53,6 +83,7 @@ if __name__=="__main__":
                         help='specify the base name of the output file.  The default is stdout')
     parser.add_argument('--title', help='Title of plot')
     parser.add_argument('--together', action="store_true", help='All matrices plotted together')
+    parser.add_argument('--mock_multiple', action="store_true", help='Plot multimple samples from mock data')
     args = parser.parse_args()
 
-    main(args.files, args.output, args.title,args.together)
+    main(args.files, args.output, args.title,args.together, args.mock_multiple)
---
+++ experiments/2013-06-18-plots/runs/2013-06-18-all-plots/add_rows_to_matrix_mock.sh
@@ -0,0 +1,1 @@
+../../src/scripts/add_rows_to_matrix_mock.sh
